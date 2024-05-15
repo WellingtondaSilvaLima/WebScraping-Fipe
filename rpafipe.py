8d0ea3806a7c42a88b57fe9ea4c2f3694bc06dee
Wellington Lima <wellingtonlima.jt@gmail.com>
2024-05-15 16:28:14 -0300

    Final
    Finalizado o programa que pega todos os veículos da tabela FIPE

--- rpafipe.py
+++ rpafipe.py
@@ -1,36 +1,14 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from pyautogui import *
-from time import *
+from time import sleep
 from tkinter import messagebox
 import pandas as pd
 
-servico = Service(ChromeDriverManager().install())
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('detach', True)
-
-navegador = webdriver.Chrome(options=options, service=servico)
-
-navegador.get('https://veiculos.fipe.org.br/#carro-comum')
-
-messagebox.showinfo(title='Início', message='Deixe aberto o site e com a aba dos carros na tela.')
-
-PAUSE = 3
-
-localizacao = locateOnScreen('imagens/carro.png')
-
-x,y = center(localizacao)
-
-click(x,y)
-
-montadoras_ano_carros = []
-
-ciclo = 0
-while ciclo < 1:
+def inicio_automacao():
   sleep(1)
   #  Clica dentro da caixa
   click(x,(y + 150))
@@ -50,47 +28,92 @@ while ciclo < 1:
 
   sleep(1)
 
+
+def pega_montadoras(navegador):
   #  Pega uma lista com todas as montadoras
   montadoras = navegador.find_element(By.ID, 'selectMarcacarro').find_elements(By.TAG_NAME, 'option')
 
   #  Limpa a lista de elementos em branco
-  tamanho = len(montadoras) - 1
-  for index in range(0, tamanho):
-    if montadoras[index].text == '':
-      del(montadoras[index])
+  montadoras = montadoras[1:]
 
-  print(len(montadoras))
+  return montadoras
 
+
+def pega_anos(navegador):
   #  Pega uma lista com todos os anos da montadora selecionada
   ano = navegador.find_element(By.ID, 'selectAnocarro').find_elements(By.TAG_NAME, 'option')
 
   #  Limpa a lista de elementos em branco
-  tamanho = len(ano) - 1
-  for index in range(0, tamanho):
-    if ano[index].text == '':
-      del(ano[index])
-  
-  print(len(ano))
+  ano = ano[1:]
+
+  return ano
 
 
-
-  print(10*'-')
-
-  for ciclo_ano in range(len(ano)):
-    #  Pega todos os modelos do ano selecionado
+def pega_modelos(navegador):
+  #  Pega todos os modelos do ano selecionado
     modelos = navegador.find_element(By.ID, 'selectAnoModelocarro').find_elements(By.TAG_NAME, 'option')
 
     #  Limpa a lista de elementos em branco
-    tamanho = len(modelos) - 1
-    for index in range(0, tamanho):
-      if modelos[index].text == '':
-        del(modelos[index])
-    
+    modelos = modelos[1:]
 
+    return modelos
+
+def seleciona_ano_seguinte():
+    keyDown('shift')
+    press('tab')
+    keyUp('shift')
+    press('tab')
+    press('down')
+    press('enter')
+    sleep(1)
+
+
+servico = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service=servico)
+
+navegador.get('https://veiculos.fipe.org.br/#carro-comum')
+
+messagebox.showinfo(title='Início', message='Ao iniciar o sistema, não poderá utilizar a máquina até terminar.')
+
+localizacao = locateOnScreen('imagens/carro.png')
+
+x,y = center(localizacao)
+
+click(x,y)
+
+montadoras_lista = []
+anos_lista = []
+modelos_lista = []
+
+inicio_automacao()
+
+montadoras = pega_montadoras(navegador)
+
+ciclo = 0
+while ciclo < len(montadoras):
+  ano = pega_anos(navegador)
+
+  for ciclo_ano in range(len(ano)):
+    modelos = pega_modelos(navegador)
+    
     for carro in range(len(modelos)):
-      # montadoras_ano_carros.append()
-      print(f'{montadoras[ciclo+1].text} | {ano[ciclo_ano].text} - {modelos[carro].text}')
+      montadoras_lista.append(montadoras[ciclo].text)  #  Adiciona a montadora na lista de montadoras
+      anos_lista.append(ano[ciclo_ano].text)  #  Adiciona o ano na lista de anos
+      modelos_lista.append(modelos[carro].text)  #  Adiciona o modelo na lista de modelos
+    
+    seleciona_ano_seguinte()
   
-  print(10*'-')
+  inicio_automacao()
   
   ciclo += 1
+
+montadoras_ano_carros = {
+        'Montadrora': montadoras_lista,
+        'Ano': anos_lista,
+        'Modelo': modelos_lista
+        }
+
+veiculos = pd.DataFrame(montadoras_ano_carros)
+
+veiculos.to_excel('FIPE.xlsx')
