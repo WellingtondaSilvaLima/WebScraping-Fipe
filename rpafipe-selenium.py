0cd4e41171d190687dd765bcd072f4503fd17bd0
Wellington Lima <wellingtonlima.jt@gmail.com>
2024-08-01 15:54:35 -0300

    Criação de funções
    Criei 3 funções para fazer a extração dos dados

--- rpafipe-selenium.py
+++ rpafipe-selenium.py
@@ -18,7 +18,74 @@ from tkinter import messagebox
 caminho = r'C:\Users\wellington\Desktop\Suporte Atendimento\projetos\APIFipe\APIFipe\chromedriver-win64\chromedriver.exe'  # Path do chromedriver
 servico = Service(caminho)  # Configura o serviço do ChromeDriver
 navegador = webdriver.Chrome(service=servico)  # Cria o navegador
-navegador.maximize_window()  # Deixa a janela maximizada
+navegador.set_window_size(930,1014)  # Define o tamanho da tela
+
+
+def extrai_veiculos(veiculos:list):
+  '''
+  Inicia a extração dos veículos da tabela FIPE
+
+  Parâmetros:
+    - veiculos: é uma das posições da tabela de veículos da FIPE (carros, caminhões ou motos)
+  
+  Retorno:
+    - Não retorna nada
+  '''
+  veiculos.click()
+  carros = veiculos.find_element(By.CLASS_NAME, 'horizontal')
+  artigos_html = carros.find_elements(By.TAG_NAME, 'article')
+  formulario = artigos_html[0].find_element(By.CLASS_NAME, 'form')
+  step_1 = formulario.find_element(By.CLASS_NAME, 'step-1')
+  step_2 = formulario.find_element(By.CLASS_NAME, 'step-2')
+  entrada_carros = step_1.find_element(By.CLASS_NAME, 'input')
+
+  entrada_carros.click()
+
+  seletor = entrada_carros.find_element(By.TAG_NAME, 'select')
+  opcoes_carros = seletor.find_elements(By.TAG_NAME, 'option')
+
+  lista_montadoras = []
+  for op in opcoes_carros:
+    if op.text == '': continue
+
+    lista_montadoras.append(op.text)
+    op.click()
+    seleciona_ano(step_2)
+
+
+def seleciona_ano(step_2):
+  entradas = step_2.find_elements(By.CLASS_NAME, 'input')
+
+  entradas[1].click()
+
+  seletor = entradas[1].find_element(By.TAG_NAME, 'select')
+  opcoes_ano = seletor.find_elements(By.TAG_NAME, 'option')
+
+  lista_anos = []
+  for op in opcoes_ano:
+    if op.text == '': continue
+
+    lista_anos.append(op.text)
+    op.click()
+    seleciona_modelo(entradas[2])
+
+
+def seleciona_modelo(entrada):
+  entrada.click()
+
+  seletor = entrada.find_element(By.TAG_NAME, 'select')
+  opcoes_modelo = seletor.find_elements(By.TAG_NAME, 'option')
+
+  lista_modelos = []
+  for op in opcoes_modelo:
+    if op.text == '': continue
+
+    lista_modelos.append(op.text)
+
+  return {
+        'Modelo': lista_modelos
+        }
+
 
 sleep(2)
 
@@ -26,26 +93,12 @@ navegador.get('https://veiculos.fipe.org.br/#carro-comum')
 
 messagebox.showinfo(title='Início', message='Pode iniciar?')
 
-identificador = ['selectMarcacarro', 'selectMarcacaminhao', 'selectMarcamoto']
 tabela_veiculos = navegador.find_element(By.CLASS_NAME, 'tab-veiculos')
 veiculos = tabela_veiculos.find_elements(By.CLASS_NAME, 'ilustra')
 
-veiculos[0].click()
-carros = veiculos[0].find_element(By.CLASS_NAME, 'horizontal')
-artigos_html = carros.find_elements(By.TAG_NAME, 'article')
-formulario = artigos_html[0].find_element(By.CLASS_NAME, 'form')
-step_1 = formulario.find_element(By.CLASS_NAME, 'step-1')
-entrada_carros = step_1.find_element(By.CLASS_NAME, 'input')
-sleep(1)
-entrada_carros.click()
-sleep(1)
-seletor = entrada_carros.find_element(By.TAG_NAME, 'select')
-opcoes_carros = seletor.find_elements(By.TAG_NAME, 'option')
-primeiro = opcoes_carros[1]
+lista_montadoras = []
+lista_anos = []
+lista_modelos = []
 
-print(primeiro.text)
-# for op in opcoes_carros:
-#   print(op.text)
-
-
+extrai_veiculos(veiculos[0], lista_montadoras, lista_anos, lista_modelos)
 
